9c69bfea1e6fc84434fad7bcb544c73a32ab1ad8
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2023-07-19 21:11:28 +0100

    created scripts that allows for generating a new private key

---
+++ server/scripts/generateKey.js
@@ -0,0 +1,5 @@
+const { secp256k1 } = require("ethereum-cryptography/secp256k1");
+const { toHex } = require("ethereum-cryptography/utils");
+
+const privateKey = secp256k1.utils.randomPrivateKey();
+console.log(`private key: ${toHex(privateKey)}`);
