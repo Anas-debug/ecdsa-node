b32c21a3b40fdae6eec13949210830bbe70069cc
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2023-08-28 00:36:40 +0100

    refractored inquiries

--- index.js
+++ index.js
@@ -1,42 +1,40 @@
 import inquirer from 'inquirer';
 import fs from 'fs';
+import path from 'path';
 
-// Function to retrieve directories in a given path
-function getDirectories(path) {
-  return fs.readdirSync(path, { withFileTypes: true })
-    .filter(dirent => dirent.isDirectory())
-    .map(dirent => dirent.name);
-}
-
-// Function to interactively choose a directory
-async function chooseDirectory(directories) {
+async function selectDirectories() {
   const questions = [
     {
-      type: 'list',
-      name: 'selectedDirectory',
-      message: 'Select a directory:',
-      choices: directories,
+      type: 'input',
+      name: 'dir1',
+      message: 'Enter path to the app front-end',
+      validate: function (input) {
+        if (fs.existsSync(input) && fs.statSync(input).isDirectory()) {
+          return true;
+        }
+        return 'Please provide a valid directory path.';
+      },
+    },
+    {
+      type: 'input',
+      name: 'dir2',
+      message: 'Enter the path to the app back-end',
+      validate: function (input) {
+        if (fs.existsSync(input) && fs.statSync(input).isDirectory()) {
+          return true;
+        }
+        return 'Please provide a valid directory path.';
+      },
     },
   ];
 
   const answers = await inquirer.prompt(questions);
-  return answers.selectedDirectory;
+  const dir1 = path.normalize(answers.dir1);
+  const dir2 = path.normalize(answers.dir2);
+
+  console.log('Selected directories:');
+  console.log('Directory 1:', dir1);
+  console.log('Directory 2:', dir2);
 }
 
-// Entry point of the CLI app
-async function main() {
-  const targetPath = './'; // Change this to the path you want to list directories from
-  const directories = getDirectories(targetPath);
-
-  if (directories.length === 0) {
-    console.log('No directories found.');
-    return;
-  }
-
-  const selectedDirectory = await chooseDirectory(directories);
-  console.log(`You selected: ${selectedDirectory}`);
-
-  // You can use the selectedDirectory variable here for further processing
-}
-
-main();
+selectDirectories();
